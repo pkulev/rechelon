904c1c7ad491c492338cc9b5d35d076dafe0e35d
Pavel Kulyov <pkulev@croc.ru>
2021-04-13 12:18:23 +0300

    Initial REPL Explorer
    Signed-off-by: Pavel Kulyov <pkulev@croc.ru>

---
+++ Tools/Explorer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Explorer.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
